a70677ac0cc0845093b09fc3b4af21aaa17b19f8
WilfredoJoseph Hiwatig <jhiwatig.dev@gmail.com>
2020-10-30 14:13:48 -0400

    Added parse input function

---
+++ server/src/utils/kataHelpers.js
@@ -0,0 +1,31 @@
+function parseInputs(args, inputSeparator = ",", typeSeparator = ":") {
+    const inputs = [];
+
+    const splitInputs = args.split(inputSeparator);
+
+   splitInputs.forEach(inputItem => {
+       const [input, type] = inputItem.split(typeSeparator);
+
+       switch(type) {
+            case "string": {
+                inputs.push(input);
+                break;
+            }
+            case "number": {
+                inputs.push(Number(input));
+                break;
+            }
+            case "boolean": {
+                inputs.push(input === 'true');
+                break;
+            }
+            case "array": {
+                inputs.push(parseInputs(input, "'", "#"));
+                break;
+            }
+       }
+   });
+
+   return inputs;
+
+}
