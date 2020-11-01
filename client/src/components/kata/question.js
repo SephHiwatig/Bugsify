050c1fbc2cfbd93f8efdca33316185f4aa9c77db
WilfredoJoseph Hiwatig <jhiwatig.dev@gmail.com>
2020-11-01 01:06:37 -0400

    Kata not viewable when not authenticated

--- client/src/components/kata/question.js
+++ client/src/components/kata/question.js
@@ -3,14 +3,17 @@ import styled from 'styled-components';
 import { AiFillLike } from "react-icons/ai";
 import ReadOnlyEditor from './editor';
 import { Link } from 'react-router-dom';
+import { useSelector } from "react-redux";
 
 const Question = ({kata}) => {
+    const isAuth = useSelector((state) => state.auth.isAuthenticated);
     return (<Wrapper>
         <Content>
             <QuestionTitle>
-                <CustomLink to="/view">
+                { isAuth && <CustomLink to="/view">
                     <span>{kata.title}</span>
-                </CustomLink>
+                </CustomLink>}
+                {!isAuth && <span>{kata.title}</span>}
             </QuestionTitle>
             { kata.title && <Info>
                 <InfoItem>Difficulty: {kata.difficulty}</InfoItem>
